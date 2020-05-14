b219b574ff0be41f77fd9c378c80f28713890f4c
pratyayj <pratyay.jaidev@gmail.com>
2020-05-14 23:48:44 +0800

    Update function with pub/sub

--- index.js
+++ index.js
@@ -5,23 +5,26 @@
  * ExpressJS req and res is utilized.
  */
 exports.analyzeReviewSentiment = (req, res) => {
-  const dotenv = require('dotenv');
-  dotenv.config();
+  //const dotenv = require('dotenv');
+  //dotenv.config();
 
   const projectId = process.env.PROJECT_ID;
   const location = process.env.LOCATION;
   const modelId = process.env.MODEL_ID;
+  const topicName = process.env.TOPIC_NAME;
 
   const {PredictionServiceClient} = require('@google-cloud/automl').v1;
+  const {PubSub} = require('@google-cloud/pubsub')
   const options={
     keyFilename:process.env.KEY_FILENAME
   };
 
   const client = new PredictionServiceClient(options);
+  const pubSubClient = new PubSub();
 
   let analysisText = req.body.text;
 
-  async function predict() {
+  async function predictAndRecord() {
     let testRequest = {
       name: client.modelPath(projectId, location, modelId),
       payload: {
@@ -37,6 +40,15 @@ exports.analyzeReviewSentiment = (req, res) => {
       "negative_score": '',
     }
 
+    let pubSubMessage = {
+      "ipAddress": req.ip,
+      "datetime": new Date()
+    }
+
+    console.log(pubSubMessage);
+    const data = JSON.stringify(pubSubMessage);
+    const dataBuffer = Buffer.from(data);
+
     const [response] = await client.predict(testRequest);
     for (const annotationPayload of response.payload) {
       if (annotationPayload.displayName === 'positive') {
@@ -48,8 +60,9 @@ exports.analyzeReviewSentiment = (req, res) => {
       // console.log(`Predicted class name: ${annotationPayload.displayName}`);
       // console.log(`Predicted class score: ${annotationPayload.classification.score}`);
     }
+    const messageId = await pubSubClient.topic(topicName).publish(dataBuffer);
     res.send(jsonResponse);
   }
 
-  predict();
+  predictAndRecord();
 };
